d9077cbe22f1a8f074fb6f94bf2b8363b108f2d1
xanderkoo <xanderkoo@gmail.com>
2020-06-26 17:28:26 -0700

    Tweak facemesh.js + calibration page

--- src/facemesh.js
+++ src/facemesh.js
@@ -50,25 +50,30 @@
         this.positionsArray = predictions[0].scaledMesh;
         const positions = this.positionsArray;
 
-        // Fit the detected eye in a rectangle
+        // Fit the detected eye in a rectangle. [20200626 xk] not clear which approach is better
         // https://raw.githubusercontent.com/tensorflow/tfjs-models/master/facemesh/mesh_map.jpg
-        var leftOriginX = Math.round(positions[130][0]);
-        var leftOriginY = Math.round(positions[27][1]);
-        var leftWidth = Math.round(positions[244][0] - leftOriginX);
-        var leftHeight = Math.round(positions[23][1] - leftOriginY);
-        var rightOriginX = Math.round(positions[464][0]);
-        var rightOriginY = Math.round(positions[257][1]);
-        var rightWidth = Math.round(positions[359][0] - rightOriginX);
-        var rightHeight = Math.round(positions[253][1] - rightOriginY);
 
-        // var leftOriginX = Math.round(Math.min(positions[247][0], positions[130][0], positions[25][0]));
-        // var leftOriginY = Math.round(Math.min(positions[247][1], positions[27][1], positions[190][1]));
-        // var leftWidth = Math.round(Math.max(positions[190][0], positions[243][0], positions[233][0]) - leftOriginX);
-        // var leftHeight = Math.round(Math.max(positions[25][1], positions[23][1], positions[112][1]) - leftOriginY);
-        // var rightOriginX = Math.round(Math.min(positions[414][0], positions[463][0], positions[453][0]));
-        // var rightOriginY = Math.round(Math.min(positions[414][1], positions[257][1], positions[467][1]));
-        // var rightWidth = Math.round(Math.max(positions[467][0], positions[359][0], positions[255][0]) - rightOriginX);
-        // var rightHeight = Math.round(Math.max(positions[341][1], positions[253][1], positions[255][1]) - rightOriginY);
+        // // Maintains a relatively stable shape of the bounding box at the cost of cutting off parts of
+        // // the eye when the eye is tilted.
+        // var leftOriginX = Math.round(positions[130][0]);
+        // var leftOriginY = Math.round(positions[27][1]);
+        // var leftWidth = Math.round(positions[243][0] - leftOriginX);
+        // var leftHeight = Math.round(positions[23][1] - leftOriginY);
+        // var rightOriginX = Math.round(positions[463][0]);
+        // var rightOriginY = Math.round(positions[257][1]);
+        // var rightWidth = Math.round(positions[359][0] - rightOriginX);
+        // var rightHeight = Math.round(positions[253][1] - rightOriginY);
+        
+        // Won't really cut off any parts of the eye, at the cost of warping the shape (i.e. height/
+        // width ratio) of the bounding box.
+        var leftOriginX = Math.round(Math.min(positions[247][0], positions[130][0], positions[25][0]));
+        var leftOriginY = Math.round(Math.min(positions[247][1], positions[27][1], positions[190][1]));
+        var leftWidth = Math.round(Math.max(positions[190][0], positions[243][0], positions[233][0]) - leftOriginX);
+        var leftHeight = Math.round(Math.max(positions[25][1], positions[23][1], positions[112][1]) - leftOriginY);
+        var rightOriginX = Math.round(Math.min(positions[414][0], positions[463][0], positions[453][0]));
+        var rightOriginY = Math.round(Math.min(positions[414][1], positions[257][1], positions[467][1]));
+        var rightWidth = Math.round(Math.max(positions[467][0], positions[359][0], positions[255][0]) - rightOriginX);
+        var rightHeight = Math.round(Math.max(positions[341][1], positions[253][1], positions[255][1]) - rightOriginY);
 
         if (leftWidth === 0 || rightWidth === 0){
           console.log('an eye patch had zero width');
